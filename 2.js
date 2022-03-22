1b3e488bbe7310e42d70791eef46fbe72a9b12fe
gas53 <45990653+GAS53@users.noreply.github.com>
2022-03-22 09:20:26 +0300

    Lesson_1

---
+++ 2.js
@@ -0,0 +1,11 @@
+/*Объявить две переменные: admin и name. Записать в name строку "Василий"; Скопировать
+значение из name в admin. Вывести admin (должно вывестись «Василий»).
+*/
+
+var admin = NaN 
+var name_ = NaN
+
+name_ = 'Василий'
+admin = name_
+
+console.log(admin)
